2f62d170736d675d37c9d259e74eddef8e936670
Juliana Nocchi Dobal <junocchi@gmail.com>
2022-12-05 15:35:35 +0000

    completed check_codeword_spec.rb

--- spec/check_codeword_spec.rb
+++ spec/check_codeword_spec.rb
@@ -1,7 +1,19 @@
 require "check_codeword"
 
 describe "check_codeword" do
+
   it "returns Correct" do
     expect(check_codeword("horse")).to eq "Correct! Come in."
   end 
+
+  it "returns Close" do
+    result = check_codeword("h..e")
+    expect(result).to eq "Close, but nope."
+  end
+
+  it "returns WRONG!" do
+    result = check_codeword("banana")
+    expect(result).to eq "WRONG!"
+  end
+
 end
