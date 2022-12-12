20e4dfb9dbc7760a46e85b39e2b3f1e5b84ebeb9
Juliana Nocchi Dobal <junocchi@gmail.com>
2022-12-12 16:15:45 +0000

    updated Challenge report_length - testing methods with equality

--- spec/report_length_spec.rb
+++ spec/report_length_spec.rb
@@ -1,15 +1,29 @@
 require "report_length"
 
-describe "report_length" do
-  it "returns length of string" do
-    expect(report_length("hello")).to eq "This string was 5 characters long."
+describe "report_length method" do
+  it "returns the length of string if given a string" do
+    result = report_length("hello")
+    expect(result).to eq "This string was 5 characters long."
   end
 
   it "returns invalid character" do
-    expect(report_length(2)).to eq "Not a string"
+    result = report_length(2)
+    expect(result).to eq "Not a string"
   end
+
 end
 
+# First version, before following the examples video:
+# require "report_length"
 
+# describe "report_length" do
+#   it "returns length of string" do
+#     expect(report_length("hello")).to eq "This string was 5 characters long."
+#   end
+
+#   it "returns invalid character" do
+#     expect(report_length(2)).to eq "Not a string"
+#   end
+# end
 # Check characters
 # If input is empty
